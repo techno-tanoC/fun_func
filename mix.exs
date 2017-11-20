05576ccae0429729d807e2f742d4532f86e15bb9
techno-tanoC <develop.tanoc@gmail.com>
2017-11-20 22:47:08 +0900

    update the library description

--- mix.exs
+++ mix.exs
@@ -7,12 +7,16 @@ defmodule FunFunc.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       name: "FunFunc",
-      description: "Fun Functions for general purpose.",
+      description: description(),
       package: package(),
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps(),
     ]
+  end
+
+  def description do
+    "Fun Functions for general purpose."
   end
 
   def package do
